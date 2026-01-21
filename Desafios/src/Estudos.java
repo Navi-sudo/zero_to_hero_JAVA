eb455a618b5166093704df4a5eae2988cd4228e1
Navi-sudo <maed.ornellas@gmail.com>
2026-01-21 20:45:44 -0300

    Desafios

---
+++ Desafios/src/Estudos.java
@@ -0,0 +1,8 @@
+public class Estudos {
+    public static void main(String[] args){
+        System.out.println("O lema de JAVA é 'WORA'");
+        System.out.println("Que significa 'Write Once, Run Anywhere'");
+        System.out.println("Que traduzido seria:");
+        System.out.println("'Escreva uma vez, Rode em qualquer lugar.'");
+    }
+}
---
+++ Desafios/src/Ok.java
@@ -0,0 +1,6 @@
+public class Ok {
+    public static void main (String[] args){
+        System.out.println("Hello, Mary Edward!");
+        System.out.println("How u doing?");
+    }
+}
